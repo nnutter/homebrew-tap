77e09357ce10a97b0f9bb74ac70004ad8f5fe83e
Nathan Nutter <nnutter@duck.com>
2026-08-11 23:57:23 -0500

    Install git-wt's zsh function and completion

--- Formula/git-wt.rb
+++ Formula/git-wt.rb
@@ -16,20 +16,24 @@ class GitWt < Formula
   def install
     ldflags = "-s -w -X main.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
+
+    zsh_completion.mkpath
+    system bin/"git-wt", "generate", "zsh", "--out", zsh_completion
   end
 
   def caveats
     <<~EOS
-      Optional zsh wrapper (wt):
-        git-wt generate zsh
-        # or: git-wt generate zsh --name wt --out $XDG_DATA_HOME/zsh/site-functions --force
+      The wt wrapper and completion were installed in:
+        #{zsh_completion}
 
-      Ensure the output directory is on fpath, then restart zsh or run compinit.
+      Ensure this directory is on fpath, then restart zsh or run compinit.
     EOS
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/git-wt --version")
     assert_match "Manage Git worktrees", shell_output("#{bin}/git-wt --help")
+    assert_path_exists zsh_completion/"wt"
+    assert_path_exists zsh_completion/"_wt"
   end
 end
